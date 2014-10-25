4fda6ef8e3a05396576fffa9c78e194d9dd34b98
polariz90 <choi156@illinois.edu>
2014-10-25 14:26:35 -0500

    i829c fixed
    i829c fixed

--- prOS/student-distrib/debug.bat
+++ prOS/student-distrib/debug.bat
@@ -1,3 +1,3 @@
 c:
 cd "c:\qemu-1.5.0-win32-sdl\"
-qemu-system-i386w.exe -hda Z:\prOS\prOS\student-distrib\mp3.img -m 256 -gdb tcp:127.0.0.1:1234 -S -name mp3
+qemu-system-i386w.exe -hda Z:\choi156\mp3\student-distrib\mp3.img -m 256 -gdb tcp:127.0.0.1:1234 -S -name mp3
