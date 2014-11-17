d44f1ee625e23e74aac0994334fa37c6028e0391
polariz90 <choi156@illinois.edu>
2014-11-16 18:03:47 -0600

    reader//writer
    reader//writer

--- prOS/student-distrib/sys_call.c
+++ prOS/student-distrib/sys_call.c
@@ -1,6 +1,16 @@
 #include "lib.h"
 #include "sys_call.h"
 #include "file.h"
+#include "x86_desc.h"
+
+
+#define space_char 32
+#define vir_mem_add 0x08000000
+#define phy_mem_add 0x800000
+#define four_mb 0x200000
+
+/* array to keep in check of process number */
+uint32_t occupied[7] = {0,0,0,0,0,0,0};
 
 /* Description:
  * system call halt.
@@ -8,6 +18,8 @@
  * 
  */
 int32_t halt(uint8_t status){
+
+	
 	return 0;
 }
 
@@ -19,35 +31,134 @@ int32_t halt(uint8_t status){
  * 
  */
 int32_t execute(const uint8_t* command){
-	
-	uint8_t buf[four_kb];
-	read_file_img(command, buf);
+
+	/* getting new pid for processes */
+	int pid = get_next_pid();
+	if(pid == -1){
+		return -1; /* fail execute */
+	}
+
 	/*Parse*/
+	uint8_t com_arr[128];
+	uint8_t arg_arr[128];
+	int i; /* loop counter */
+	/* special case check */
+	if(*command == NULL){
+		/* case empty string */
+		return -1;
+	}
+	if(*command == space_char){
+		/* case single space string */
+		return -1; 
+	}
+	i = 0;
+	while(*command != space_char){/* copying command */
+		com_arr[i] = *command;
+		command ++;
+	}
+	i = 0;
+	while(*command != '\n'){/* copying argument */
+		arg_arr[i] = *command;
+		command ++;
+	}
+
+
 
 	/*Excutable check*/
+	uint8_t buf[four_kb];
+	read_file_img(com_arr, buf);
+	uint8_t ELF[4];
+	ELF[0]=0x7f;
+	ELF[1]=0x45;
+	ELF[2]=0x4c;
+	ELF[3]=0x46;
+	if(strncmp((uint8_t*)buf, (uint8_t*)ELF, 4)) {printf("not Excutable!!\n");}
 
 	/*Paging*/
+	map_4KB_page(pid, vir_mem_add, phy_mem_add+(pid-1)*four_mb, 1);
 
 	/*File loader*/
+	load_file_img(com_arr);
 
 	/*new PCB*/
+	pcb* new_pcb = add_process_stack(pid);
+
+	/* filling PCB with stuff */
+	new_pcb->pid = pid;
+	strcpy((int8_t*)new_pcb->arg, (int8_t*)arg_arr);
+	new_pcb->parent_eip=tss.eip;
 
 	/*context switch*/
-	uint8_t entry_point[4];
-	memcpy(entry_point, buf+23, 4);
+	uint32_t entry_point;
+	memcpy(&entry_point, buf+23, 4);
+	//printf("%s\n", entry_point);
+	asm("pushal");
+
 	//asm("movl %eip, %eax\n\t" "pushl %eax\n\t");
-	asm("push %eax");
+	//asm("pushl %%eax" : : : "eax");
+	asm volatile("pushl %%ebx        \n      \
+			pushl %%ecx        \n      \
+			pushl %%edx        \n      \
+			pushl $0x00c00000        \n      \
+			pushl %%ecx"                    \
+			: 
+			: "b"(entry_point), "c"(USER_CS), "d"(tss.eflags)
+			: "eax" );
+
+	asm("iret");
+/*	asm("pushl %%ebx	;
+		pushl %%ebx"
+		 : :  : "eax" );
+*/
+	asm("popal");
 
 	return 0;
 }
 
-/* Description:
- * system call read.
+/*
+ * read()
  *
- * 
+ * Reads 'nbytes' bytes into 'buf' from the file corresponding to the
+ * given 'fd'.
+ *
+ * Inputs:
+ * fd: file descriptor
+ * buf: buffer to read from
+ * nbytes: number of bytes to read from
+ *
+ * Retvals: number of bytes read
  */
 int32_t read(int32_t fd, void* buf, int32_t nbytes){
-	return 0;
+
+	sti();
+	int bytesread;
+	int pid = get_next_pid();
+
+	pcb * new_pcb = add_process_stack(pid);
+
+	if( fd < 0 || fd > 7 || buf == NULL || new_pcb->file_descriptor[fd].flags == 0 )
+	{
+			return -1;
+	}
+
+	uint8_t* filename = new_pcb->filenames[fd];
+	uint32_t fileposition = new_pcb->file_descriptor[fd].file_pos;
+
+	asm volatile("pushl %0          ;"
+				 "pushl %1              ;"
+				 "pushl %2              ;"
+				 "pushl %3              ;"
+				 "call  *%4             ;"
+				 :
+				 : "g" (fileposition), "g" ((int32_t)filename), "g" (nbytes), "g" ((int32_t)buf),
+				   "g" (new_pcb->file_descriptor[fd].file_opt_ptr[1]));
+							 
+	asm volatile("movl %%eax, %0":"=g"(bytesread));
+	asm volatile("addl $16, %esp    ;");
+
+	new_pcb->file_descriptor[fd].file_pos += bytesread;
+	
+	return bytesread;
 }
 
 
@@ -57,6 +168,23 @@ int32_t read(int32_t fd, void* buf, int32_t nbytes){
  * 
  */
 int32_t write(int32_t fd, void* buf, int32_t nbytes){
+	int pid = get_next_pid();
+	pcb * new_pcb = add_process_stack(pid);
+	
+	if( fd < 0 || fd > 7 || buf == NULL || new_pcb->filedescriptor[fd].flags == 0 )
+	{
+			return -1;
+	}
+
+
+	asm volatile("pushl %0          ;"
+				 "pushl %1              ;"
+				 "call  *%2             ;"
+				 "leave					;"
+				 "ret					;"
+				 :
+				 : "g" (nbytes), "g" ((int32_t )buf), "g" (new_pcb->filedescriptor[fd].file_opt_ptr[2]));
+							 
 	return 0;
 }
 
@@ -135,7 +263,27 @@ int32_t sigreturn(void){
 void sys_call_handler(){
 	asm("pushal");
 	printf("system call handle!!\n");
-	int temp;
-	temp=execute("ls");
+	int32_t temp;
+	temp = execute("shell arghaha");
 	asm("popal;leave;iret");
+}
+
+/**
+  * get next pid
+  * INPUT: none
+  * OUTPUT: pid for the new process
+  * SIDE EFFECT: none
+  */
+uint32_t get_next_pid(void){
+	int i = 0; /* loop counter */
+	while(i < 7){
+		if(occupied[i] == 0){/* case avaliable*/
+			occupied[i] = 1;
+			return i;
+		}
+		else{
+			i++;
+		}
+	}
+	return -1;
 }
